ef7b674e30d4c81ee019fce523065156f6053e25
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-11 13:13:04 -0800

    Add getComputerChoice to randomly select weapon

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const weapons = ["Rock", "Paper", "Scissors"];
+    let weapon = weapons [Math.floor(Math.random() * weapons.length)];
+    return weapon;
+}
+
+console.log(getComputerChoice());
