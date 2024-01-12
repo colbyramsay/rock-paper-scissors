6afa4e3656b2888176cb8ea819dab5760f0ee288
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-11 19:46:40 -0800

    Add conditional statements

--- javascript.js
+++ javascript.js
@@ -1,7 +1,34 @@
 function getComputerChoice() {
     const weapons = ["Rock", "Paper", "Scissors"];
     let weapon = weapons [Math.floor(Math.random() * weapons.length)];
-    return weapon;
+    return weapon.toLowerCase();
 }
 
-console.log(getComputerChoice());
+let userWeapon = prompt("Please enter Rock, Paper, or Scissors").toLowerCase();
+
+let playerSelection = userWeapon;
+console.log("Player:",playerSelection);
+let computerSelection = getComputerChoice();
+console.log("Computer:",computerSelection);
+
+if (computerSelection === "rock" && playerSelection === "rock") {
+    console.log("We have a tie!");
+} else if (computerSelection === "rock" && playerSelection === "paper") {
+        console.log("Player wins! Paper beats rock.");
+} else if (computerSelection === "rock" && playerSelection === "scissors") {
+        console.log("Computer wins! Rock beats scissors.");
+} else if (computerSelection === "paper" && playerSelection === "paper") {
+    console.log("We have a tie!");
+} else if (computerSelection === "paper" && playerSelection === "scissors") {
+        console.log("Player wins! Scissors beat paper.");
+} else if (computerSelection === "paper" && playerSelection === "rock") {
+        console.log("Computer wins! Paper beats rock.");
+} else if (computerSelection === "scissors" && playerSelection === "scissors") {
+    console.log("We have a tie!");
+} else if (computerSelection === "scissors" && playerSelection === "rock") {
+        console.log("Player wins! Rock beats scissors.");
+} else if (computerSelection === "scissors" && playerSelection === "paper") {
+        console.log("Computer wins! Scissors beats paper.");
+} else {
+    console.log("Invalid entry.");
+}
